9cf8aa341bf0f0304a195c0c7a2a797ccb79dab7
Darrin <125dperez@frhsd.com>
2024-01-05 10:38:56 -0500

    finished

--- js/games.js
+++ js/games.js
@@ -22,8 +22,78 @@ function intialize() {
             let title = document.createElement("span");
             title.id = r.toString() + "-" + c.toString();
             title.classList.add("title");
-            title.innerText = "p";
+            title.innerText = "";
             document.getElementById("board").appendChild(title)
         }
     }
+    // Listen for Key Press
+    document.addEventListener("keyup", (e) => {
+    if(game0ver) return;
+
+    // alert(e,code);
+    if("KeyA" <= e.code && e.code <= "KeyZ"){
+    if(col < width){
+        let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+        if(currentTile.innerText == "")
+        {
+            currentTile.innerText = e.code[3];
+            col++;
+        }
+    }
 }
+        else if(e.code == "Backspace"){
+            if(0 < col && col <= width) {
+                col--;
+            }
+            let currentTile = document.getElementById(row.toString() + "-" + col.toString()); 
+            if (currentTile.innerText == "") {
+                currentTile.innerText = e.code[3]
+                col += 1;
+             }
+        
+    
+    }
+    else if (e.code == "Backspace") {
+        if (0 < col && col <= width) {
+            col -=1;
+        }
+        let currentTile = document.getElementById(row.toString() +
+         "-" + col.toString());
+        currentTile.innerText = "";
+    }
+    
+    else if (e.code == "Enter") {
+        update();
+        row++;
+        col=0;
+    }
+    if (!gameOver && row == height) {
+        game0ver = true;
+        document.getElementById("answer").innerText = word;
+    }
+    })
+}
+    function update() {
+        let correct = 0;
+        for (let c = 0; c < width; c++) {
+            let title = document.getElementById(row.toString() + "-" + c);
+            let letter = title.innerText;
+
+            // Correct position?
+            if (word[c] == letter) {
+                title.classList.add("correct");
+                correct++;
+            }
+            else if (word.includes(letter)) {
+                title.classList.add("present");
+            }
+            else {
+                title.classList.add("absent");
+            }
+
+            if (correct == width) {
+                gameOver = true;
+                
+            }
+        }
+    }
